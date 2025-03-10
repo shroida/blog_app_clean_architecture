5c7bc7257c5469b6714faf5cebc25e8944822159
shroida <shroida012@gmail.com>
2025-03-10 15:44:59 +0200

    AppTextStyles

---
+++ lib/core/styles/app_text_styles.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle headline1 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle headline2 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle bodyText = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle caption = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: Colors.grey,
+  );
+}
