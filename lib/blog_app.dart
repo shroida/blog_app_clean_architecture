59fe83f25643a9a539a5bdfd1a3cb0e48bb51083
shroida <shroida012@gmail.com>
2025-03-10 15:34:57 +0200

    call signup in blog app

--- lib/blog_app.dart
+++ lib/blog_app.dart
@@ -1,10 +1,18 @@
+import 'package:blog_clean_architecture/features/auth/presentation/signup_page.dart';
 import 'package:flutter/material.dart';
 
 class BlogApp extends StatelessWidget {
-const BlogApp({ super.key });
+  const BlogApp({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return const Placeholder();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true),
+      home: const SignupPage(),
+    );
   }
 }
