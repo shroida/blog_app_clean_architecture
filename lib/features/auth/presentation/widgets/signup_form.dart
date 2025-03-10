f502a0625575e5b97d0e1a028e740fafd5aadaf0
shroida <shroida012@gmail.com>
2025-03-10 16:02:37 +0200

    create signup form

---
+++ lib/features/auth/presentation/widgets/signup_form.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SignupForm extends StatefulWidget {
+  const SignupForm({super.key});
+
+  @override
+  SignupFormState createState() => SignupFormState();
+}
+
+class SignupFormState extends State<SignupForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: ,
+    );
+  }
+}
