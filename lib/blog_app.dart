54435a32c1d1c2bd98ff2c9e3fb767af14c1ad62
shroida <shroida012@gmail.com>
2025-03-10 15:19:41 +0200

    init app to be ready

---
+++ lib/blog_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BlogApp extends StatelessWidget {
+const BlogApp({ super.key });
+
+  @override
+  Widget build(BuildContext context){
+    return const Placeholder();
+  }
+}
