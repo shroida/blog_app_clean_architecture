4cfba3efa8cb6aafe3b9d2db876ee97cc964b792
shroida <shroida012@gmail.com>
2025-03-11 11:14:10 +0200

    SignupPage form

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,8 +1,4 @@
-import 'package:blog_clean_architecture/core/theme/app_color.dart';
-import 'package:blog_clean_architecture/features/auth/presentation/pages/login_page.dart';
-import 'package:blog_clean_architecture/features/auth/presentation/widgets/auth_field.dart';
-import 'package:blog_clean_architecture/features/auth/presentation/widgets/auth_gradient_button.dart';
-import 'package:blog_clean_architecture/features/auth/presentation/widgets/header_texts_signup.dart';
+import 'package:blog_clean_architecture/features/auth/presentation/widgets/form_signup.dart';
 import 'package:flutter/material.dart';
 
 class SignupPage extends StatefulWidget {
@@ -18,48 +14,24 @@ class _SignupPageState extends State<SignupPage> {
   final nameController = TextEditingController();
   final passwordController = TextEditingController();
   @override
+  void dispose() {
+    emailController.dispose();
+    nameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-          child: SingleChildScrollView(
-            child: Form(
-              key: formKey,
-              child: Column(
-                children: [
-                  const HeaderTextsSignup(),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  AuthField(
-                    controller: nameController,
-                    hinText: 'Name',
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  AuthField(
-                    controller: emailController,
-                    hinText: 'Email',
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  AuthField(
-                    controller: passwordController,
-                    hinText: 'Password',
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  AuthGradientButton(buttonText: 'Sign Up', onPressed: () {}),
-                  const SizedBox(height: 20),
-                  const NavigateToSignUp(),
-                ],
-              ),
-            ),
-          ),
+          child: FormSignUp(
+              formKey: formKey,
+              nameController: nameController,
+              emailController: emailController,
+              passwordController: passwordController),
         ),
       ),
     );
