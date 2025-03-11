b98e7d4754ef3833b64cc2922b06dbaf22071e9c
shroida <shroida012@gmail.com>
2025-03-11 11:56:52 +0200

    navigate to login & signuo

---
+++ lib/features/auth/presentation/widgets/form_login.dart
@@ -0,0 +1,55 @@
+import 'package:blog_clean_architecture/features/auth/presentation/widgets/auth_field.dart';
+import 'package:blog_clean_architecture/features/auth/presentation/widgets/auth_gradient_button.dart';
+import 'package:blog_clean_architecture/features/auth/presentation/widgets/header_texts_signup.dart';
+import 'package:blog_clean_architecture/features/auth/presentation/widgets/navigate_to_signup.dart';
+import 'package:flutter/material.dart';
+
+class FormSignUp extends StatelessWidget {
+  const FormSignUp({
+    super.key,
+    required this.formKey,
+    required this.nameController,
+    required this.emailController,
+    required this.passwordController,
+  });
+
+  final GlobalKey<FormState> formKey;
+  final TextEditingController nameController;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Form(
+        key: formKey,
+        child: Column(
+          children: [
+            const HeaderTextsLogin(),
+            const SizedBox(
+              height: 30,
+            ),
+            AuthField(
+              controller: emailController,
+              hinText: 'Email',
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            AuthField(
+              controller: passwordController,
+              hinText: 'Password',
+              isObscureText: true,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            AuthGradientButton(buttonText: 'Login', onPressed: () {}),
+            const SizedBox(height: 20),
+            const NavigateToSignUp(),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/auth/presentation/widgets/navigate_to_signup.dart
+++ lib/features/auth/presentation/widgets/navigate_to_signup.dart
@@ -1,18 +1,25 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:blog_clean_architecture/features/auth/presentation/pages/signup_page.dart';
+import 'package:flutter/material.dart';
+
 import 'package:blog_clean_architecture/core/theme/app_color.dart';
 import 'package:blog_clean_architecture/features/auth/presentation/pages/login_page.dart';
-import 'package:flutter/material.dart';
 
 class NavigateToSignUp extends StatelessWidget {
   const NavigateToSignUp({
     super.key,
+    this.navigateToLogin = true,
   });
-
+  final bool navigateToLogin;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) => const LoginPage()));
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    navigateToLogin ? const LoginPage() : const SignupPage()));
       },
       child: RichText(
         text: TextSpan(
