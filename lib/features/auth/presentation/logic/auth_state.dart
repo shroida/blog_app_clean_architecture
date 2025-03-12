0dbeec242fa1cc3bc8cd8ab007935091445b4467
shroida <shroida012@gmail.com>
2025-03-12 15:34:06 +0200

    AuthState update

--- lib/features/auth/presentation/logic/auth_state.dart
+++ lib/features/auth/presentation/logic/auth_state.dart
@@ -4,6 +4,28 @@ class AuthInitial extends AuthState {}
 
 class AuthLoading extends AuthState {}
 
-class AuthSuccess extends AuthState {}
+class AuthSuccess extends AuthState {
+  final String userId;
+  AuthSuccess({required this.userId});
+}
 
-class AuthFailure extends AuthState {}
+class SignupSuccess extends AuthSuccess {
+  SignupSuccess({required super.userId});
+}
+
+class LoginSuccess extends AuthSuccess {
+  LoginSuccess({required super.userId});
+}
+
+class AuthFailure extends AuthState {
+  final String message;
+  AuthFailure({required this.message});
+}
+
+class SignupFailure extends AuthFailure {
+  SignupFailure({required super.message});
+}
+
+class LoginFailure extends AuthFailure {
+  LoginFailure({required super.message});
+}
