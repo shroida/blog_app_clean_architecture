9073aa5114313b49ff8585080412615e35d7ec85
shroida <shroida012@gmail.com>
2025-03-12 15:55:03 +0200

    AuthCubit

---
+++ lib/features/auth/presentation/logic/auth_cubit.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:blog_clean_architecture/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:blog_clean_architecture/features/auth/presentation/logic/auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  final UserSignUp _userSignUp;
+  AuthCubit(this._userSignUp) : super(AuthInitial());
+
+  Future<void> signup({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    emit(AuthLoading()); // Emit loading state
+    try {
+      final response = await _userSignUp.call(
+        UserSignUpParams(email: email, password: password, name: name),
+      );
+
+      response.fold(
+        (failure) =>
+            emit(AuthFailure(message: failure.message)), // Emit failure state
+        (userId) => emit(AuthSuccess(userId: userId)), // Emit success state
+      );
+    } catch (e) {
+      emit(AuthFailure(message: e.toString()));
+    }
+  }
+}
