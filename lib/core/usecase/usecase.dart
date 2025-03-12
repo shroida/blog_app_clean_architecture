8a053f6ba64fed5952d5c8ea35809f3c6165b6a5
shroida <shroida012@gmail.com>
2025-03-12 15:28:41 +0200

    UserSignUp done

--- lib/core/usecase/usecase.dart
+++ lib/core/usecase/usecase.dart
@@ -1,6 +1,6 @@
 import 'package:blog_clean_architecture/core/error/failure.dart';
 import 'package:fpdart/fpdart.dart';
 
-abstract interface class Usecase<SuccessType,Params> {
+abstract interface class UseCase<SuccessType,Params> {
   Future<Either<Failure, SuccessType>> call(Params params);
 }
--- lib/features/auth/domain/usecases/user_sign_up.dart
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -1,3 +1,24 @@
-class UserSignUp {
-  
+import 'package:blog_clean_architecture/core/error/failure.dart';
+import 'package:blog_clean_architecture/core/usecase/usecase.dart';
+import 'package:blog_clean_architecture/features/auth/domain/repository/auth_repo.dart';
+import 'package:fpdart/fpdart.dart';
+
+class UserSignUp implements UseCase<String, UserSignUpParams> {
+  final AuthRepo authRepo;
+
+  UserSignUp(this.authRepo);
+  @override
+  Future<Either<Failure, String>> call(params) async {
+    return await authRepo.signup(
+        name: params.name, email: params.email, password: params.password);
+  }
+}
+
+class UserSignUpParams {
+  final String email;
+  final String password;
+  final String name;
+
+  UserSignUpParams(
+      {required this.email, required this.password, required this.name});
 }
