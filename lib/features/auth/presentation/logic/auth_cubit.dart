a8acc97114fe7e93c0ccb79c3f442818e9a98d18
shroida <shroida012@gmail.com>
2025-03-17 13:14:16 +0200

    login in authCubit

--- lib/features/auth/presentation/logic/auth_cubit.dart
+++ lib/features/auth/presentation/logic/auth_cubit.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:blog_clean_architecture/features/auth/domain/usecases/user_login.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:blog_clean_architecture/features/auth/domain/usecases/user_sign_up.dart';
@@ -6,7 +7,8 @@ import 'package:blog_clean_architecture/features/auth/presentation/logic/auth_st
 
 class AuthCubit extends Cubit<AuthState> {
   final UserSignUp _userSignUp;
-  AuthCubit(this._userSignUp) : super(AuthInitial());
+  final UserLogin _userLogin;
+  AuthCubit(this._userSignUp, this._userLogin) : super(AuthInitial());
 
   Future<void> signup({
     required String name,
@@ -24,6 +26,18 @@ class AuthCubit extends Cubit<AuthState> {
             emit(AuthFailure(message: failure.message)), // Emit failure state
         (user) => emit(AuthSuccess(user: user)), // Emit success state
       );
+    } catch (e) {
+      emit(AuthFailure(message: e.toString()));
+    }
+  }
+
+  Future<void> login({required String email, required String password}) async {
+    emit(AuthLoading()); // Emit loading state
+    try {
+      final response = await _userLogin
+          .call(UserLoginParams(email: email, password: password));
+      response.fold((failure) => emit(AuthFailure(message: failure.message)),
+          (user) => emit(AuthSuccess(user: user)));
     } catch (e) {
       emit(AuthFailure(message: e.toString()));
     }
