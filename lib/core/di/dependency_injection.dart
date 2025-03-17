b2e6a2880103e3c9c575bc152f5977801ed71c11
shroida <shroida012@gmail.com>
2025-03-17 13:27:24 +0200

    blocConsumer for button

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -2,6 +2,7 @@ import 'package:blog_clean_architecture/core/constant/constant.dart';
 import 'package:blog_clean_architecture/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:blog_clean_architecture/features/auth/data/repositories/auth_repo_impl.dart';
 import 'package:blog_clean_architecture/features/auth/domain/repository/auth_repo.dart';
+import 'package:blog_clean_architecture/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_clean_architecture/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:blog_clean_architecture/features/auth/presentation/logic/auth_cubit.dart';
 import 'package:get_it/get_it.dart';
@@ -20,5 +21,6 @@ void _initAuth() {
       () => AuthRemoteDataSourceImpl(getIt()));
   getIt.registerFactory<AuthRepo>(() => AuthRepoImpl(getIt()));
   getIt.registerFactory(() => UserSignUp(getIt()));
+  getIt.registerFactory(() => UserLogin(getIt()));
   getIt.registerLazySingleton(() => AuthCubit(getIt(), getIt()));
 }
--- lib/features/auth/presentation/widgets/form_login.dart
+++ lib/features/auth/presentation/widgets/form_login.dart
@@ -1,8 +1,11 @@
+import 'package:blog_clean_architecture/features/auth/presentation/logic/auth_cubit.dart';
+import 'package:blog_clean_architecture/features/auth/presentation/logic/auth_state.dart';
 import 'package:blog_clean_architecture/features/auth/presentation/widgets/auth_field.dart';
 import 'package:blog_clean_architecture/features/auth/presentation/widgets/auth_gradient_button.dart';
 import 'package:blog_clean_architecture/features/auth/presentation/widgets/header_texts_login.dart';
 import 'package:blog_clean_architecture/features/auth/presentation/widgets/navigate_to_signup.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FormLogin extends StatelessWidget {
   const FormLogin({
@@ -19,36 +22,56 @@ class FormLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Form(
-        key: formKey,
-        child: Column(
-          children: [
-            const HeaderTextsLogin(),
-            const SizedBox(
-              height: 30,
+      child: BlocConsumer<AuthCubit, AuthState>(
+        listener: (context, state) {
+        },
+        builder: (context, state) {
+          bool isLoading = state is AuthLoading;
+          bool isError = state is AuthFailure;
+          bool isSuccess = state is AuthSuccess;
+          return Form(
+            key: formKey,
+            child: Column(
+              children: [
+                const HeaderTextsLogin(),
+                const SizedBox(
+                  height: 30,
+                ),
+                AuthField(
+                  controller: emailController,
+                  hinText: 'Email',
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                AuthField(
+                  controller: passwordController,
+                  hinText: 'Password',
+                  isObscureText: true,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                AuthGradientButton(
+                    isError: isError,
+                    isLoading: isLoading,
+                    isSuccess: isSuccess,
+                    buttonText: 'Login',
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        context.read<AuthCubit>().login(
+                            email: emailController.text,
+                            password: passwordController.text);
+                      }
+                    }),
+                const SizedBox(height: 20),
+                const NavigateTo(
+                  navigateToLogin: false,
+                ),
+              ],
             ),
-            AuthField(
-              controller: emailController,
-              hinText: 'Email',
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            AuthField(
-              controller: passwordController,
-              hinText: 'Password',
-              isObscureText: true,
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            AuthGradientButton(buttonText: 'Login', onPressed: () {}),
-            const SizedBox(height: 20),
-            const NavigateTo(
-              navigateToLogin: false,
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
