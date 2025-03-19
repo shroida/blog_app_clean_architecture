3023ffaf3d72b7292f2848e18d583c1f154ad065
shroida <shroida012@gmail.com>
2025-03-19 14:45:58 +0200

    initState to check user logged in or not

--- lib/blog_app.dart
+++ lib/blog_app.dart
@@ -1,9 +1,22 @@
 import 'package:blog_clean_architecture/core/theme/app_theme.dart';
+import 'package:blog_clean_architecture/features/auth/presentation/logic/auth_cubit.dart';
 import 'package:blog_clean_architecture/features/auth/presentation/pages/signup_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class BlogApp extends StatelessWidget {
+class BlogApp extends StatefulWidget {
   const BlogApp({super.key});
+
+  @override
+  State<BlogApp> createState() => _BlogAppState();
+}
+
+class _BlogAppState extends State<BlogApp> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<AuthCubit>().isUserLoggedIn();
+  }
 
   @override
   Widget build(BuildContext context) {
