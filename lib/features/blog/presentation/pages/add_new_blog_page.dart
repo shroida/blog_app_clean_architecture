eff89c92b6b7b536cec604aa7d835b7dae1035c1
shroida <shroida012@gmail.com>
2025-03-21 16:24:42 +0200

    BlogEditor called in add new blog

--- lib/features/blog/presentation/pages/add_new_blog_page.dart
+++ lib/features/blog/presentation/pages/add_new_blog_page.dart
@@ -1,6 +1,7 @@
 import 'package:blog_clean_architecture/core/constant/topics_constants.dart';
 import 'package:blog_clean_architecture/core/theme/app_color.dart';
 import 'package:blog_clean_architecture/features/blog/presentation/widgets/app_bar_add_new.dart';
+import 'package:blog_clean_architecture/features/blog/presentation/widgets/blog_editor.dart';
 import 'package:blog_clean_architecture/features/blog/presentation/widgets/dotted_border_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -15,52 +16,70 @@ class AddNewBlogPage extends StatefulWidget {
 }
 
 class AddNewBlogPageState extends State<AddNewBlogPage> {
+  final titleController = TextEditingController();
+  final contentController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
   List<String> selectedTopics = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const AppBarAddNew(),
-      body: Column(
-        children: [
-          const DottedBorderWidget(),
-          const SizedBox(height: 20),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: TopicsConstants.topics
-                  .map(
-                    (e) => Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: GestureDetector(
-                        onTap: () {
-                          if (selectedTopics.contains(e)) {
-                            selectedTopics.remove(e);
-                          } else {
-                            selectedTopics.add(e);
-                          }
-                          setState(() {});
-                        },
-                        child: Chip(
-                          label: Text(e),
-                          color: selectedTopics.contains(e)
-                              ? const WidgetStatePropertyAll(
-                                  AppColor.gradient1,
-                                )
-                              : null,
-                          side: selectedTopics.contains(e)
-                              ? null
-                              : const BorderSide(
-                                  color: AppColor.borderColor,
-                                ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const DottedBorderWidget(),
+              const SizedBox(height: 20),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: TopicsConstants.topics
+                      .map(
+                        (e) => Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: GestureDetector(
+                            onTap: () {
+                              if (selectedTopics.contains(e)) {
+                                selectedTopics.remove(e);
+                              } else {
+                                selectedTopics.add(e);
+                              }
+                              setState(() {});
+                            },
+                            child: Chip(
+                              label: Text(e),
+                              color: selectedTopics.contains(e)
+                                  ? const WidgetStatePropertyAll(
+                                      AppColor.gradient1,
+                                    )
+                                  : null,
+                              side: selectedTopics.contains(e)
+                                  ? null
+                                  : const BorderSide(
+                                      color: AppColor.borderColor,
+                                    ),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                  )
-                  .toList(),
-            ),
+                      )
+                      .toList(),
+                ),
+              ),
+              const SizedBox(height: 10),
+              BlogEditor(
+                controller: titleController,
+                hintText: 'Blog title',
+              ),
+              const SizedBox(height: 10),
+              BlogEditor(
+                controller: contentController,
+                hintText: 'Blog content',
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
