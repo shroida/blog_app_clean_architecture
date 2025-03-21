e353815d8a370e7b814c21f08b01f712fc55f7ce
shroida <shroida012@gmail.com>
2025-03-21 16:09:20 +0200

    DottedBorder

--- lib/features/blog/presentation/pages/add_new_blog_page.dart
+++ lib/features/blog/presentation/pages/add_new_blog_page.dart
@@ -1,3 +1,5 @@
+import 'package:blog_clean_architecture/core/theme/app_color.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class AddNewBlogPage extends StatefulWidget {
@@ -24,7 +26,37 @@ class AddNewBlogPageState extends State<AddNewBlogPage> {
           ),
         ],
       ),
-      body: Column(),
+      body: Column(
+        children: [
+          DottedBorder(
+            color: AppColor.borderColor,
+            dashPattern: const [10, 4],
+            radius: const Radius.circular(10),
+            borderType: BorderType.RRect,
+            strokeCap: StrokeCap.round,
+            child: const SizedBox(
+              height: 150,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.folder_open,
+                    size: 40,
+                  ),
+                  SizedBox(height: 15),
+                  Text(
+                    'Select your image',
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
