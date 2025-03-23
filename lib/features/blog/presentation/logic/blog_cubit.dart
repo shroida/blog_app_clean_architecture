417b602d43834a430b6589a572500eae5c111f06
shroida <shroida012@gmail.com>
2025-03-23 14:36:53 +0200

    BlogRepositoryImpl

--- lib/features/blog/presentation/logic/blog_cubit.dart
+++ lib/features/blog/presentation/logic/blog_cubit.dart
@@ -1,6 +1,32 @@
+import 'dart:io';
+
+import 'package:blog_clean_architecture/features/blog/domain/usecase/upload_blog.dart';
 import 'package:blog_clean_architecture/features/blog/presentation/logic/blog_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BlogCubit extends Cubit<BlogState> {
-  BlogCubit() : super(BlogInitial());
+  final UploadBlog uploadBlog;
+  BlogCubit(this.uploadBlog) : super(BlogInitial());
+  void _onUploadBlog({
+    required String posterId,
+    required String title,
+    required String content,
+    required File image,
+    required List<String> topics,
+  }) async {
+    emit(BlogLoading()); // حالة التحميل
+    final res = await uploadBlog.call(
+      UploadBlogParams(
+        posterId: posterId,
+        title: title,
+        content: content,
+        image: image,
+        topics: topics,
+      ),
+    );
+    res.fold(
+      (failure) => emit(BlogFailure(failure.toString())),
+      (blog) => emit(BlogSuccess(blog)),
+    );
+  }
 }
