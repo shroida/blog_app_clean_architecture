46b8404a6c09acc128043bbd845ac7f00e0fa095
shroida <shroida012@gmail.com>
2025-03-23 14:57:47 +0200

    GetIt

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -7,12 +7,18 @@ import 'package:blog_clean_architecture/features/auth/domain/usecases/current_us
 import 'package:blog_clean_architecture/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_clean_architecture/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:blog_clean_architecture/features/auth/presentation/logic/auth_cubit.dart';
+import 'package:blog_clean_architecture/features/blog/data/datasources/blog_remote_data_source.dart';
+import 'package:blog_clean_architecture/features/blog/data/repositoires/blog_repo_impl.dart';
+import 'package:blog_clean_architecture/features/blog/domain/repositories/blog_repository.dart';
+import 'package:blog_clean_architecture/features/blog/domain/usecase/upload_blog.dart';
+import 'package:blog_clean_architecture/features/blog/presentation/logic/blog_cubit.dart';
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 final getIt = GetIt.instance;
 Future<void> setUpGetIt() async {
   _initAuth();
+  _initBlog();
   final supabase =
       await Supabase.initialize(anonKey: anonKey, url: supabaseUrl);
   getIt.registerLazySingleton(() => supabase.client);
@@ -28,4 +34,16 @@ void _initAuth() {
   getIt.registerFactory(() => AppUserCubit());
   getIt.registerLazySingleton(
       () => AuthCubit(getIt(), getIt(), getIt(), getIt()));
+}
+
+void _initBlog() {
+  getIt.registerFactory<BlogRemoteDataSource>(
+      () => BlogRemoteDataSourceImpl(getIt()));
+  getIt.registerFactory<BlogRepository>(
+      () => BlogRepositoryImpl(getIt(), getIt(), getIt()));
+  getIt.registerFactory(() => UploadBlog(getIt()));
+  getIt.registerFactory(() => AppUserCubit());
+  getIt.registerLazySingleton(() => BlogCubit(
+        getIt(),
+      ));
 }
