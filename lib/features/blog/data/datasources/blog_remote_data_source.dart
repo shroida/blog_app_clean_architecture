9789d72780e770e9733a59f3db313aff87ea92a1
shroida <shroida012@gmail.com>
2025-03-23 13:44:56 +0200

    BlogModel toJson & fromJson

---
+++ lib/features/blog/data/datasources/blog_remote_data_source.dart
@@ -0,0 +1,23 @@
+import 'package:blog_clean_architecture/core/error/exceptions.dart';
+import 'package:blog_clean_architecture/features/blog/data/models/blog_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class BlogRemoteDataSource {
+  Future<BlogModel> uploadBlog(BlogModel blog);
+}
+
+class BlogRemoteDataSourceImpl implements BlogRemoteDataSource {
+  final SupabaseClient supabaseClient;
+
+  BlogRemoteDataSourceImpl(this.supabaseClient);
+  @override
+  Future<BlogModel> uploadBlog(BlogModel blog) async {
+    try {
+      final blogData =
+          await supabaseClient.from('blogs').insert({blog.toJson()}).select();
+      return BlogModel.fromJson(blogData.first);
+    } catch (e) {
+      throw ServerExceptions(e.toString());
+    }
+  }
+}
