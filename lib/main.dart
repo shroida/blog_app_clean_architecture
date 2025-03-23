4470029d50e351dcb47d0445c74adedcae0baeae
shroida <shroida012@gmail.com>
2025-03-23 14:58:38 +0200

    call cubit in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:blog_clean_architecture/blog_app.dart';
 import 'package:blog_clean_architecture/core/common/cubits/cubit/app_user_cubit.dart';
 import 'package:blog_clean_architecture/core/di/dependency_injection.dart';
 import 'package:blog_clean_architecture/features/auth/presentation/logic/auth_cubit.dart';
+import 'package:blog_clean_architecture/features/blog/presentation/logic/blog_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -12,7 +13,8 @@ void main() async {
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider(create: (_) => getIt<AppUserCubit>()),
-      BlocProvider(create: (_) => getIt<AuthCubit>())
+      BlocProvider(create: (_) => getIt<AuthCubit>()),
+      BlocProvider(create: (_) => getIt<BlogCubit>())
     ],
     child: const BlogApp(),
   ));
